c922198b3d323ced54d1f2d40e6853ac62bc14cb
brichard01 <147384974+brichard01@users.noreply.github.com>
2023-11-30 11:58:14 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        /*JFrame frame = new JFrame("Calculator");
-        frame.setContentPane(new inter().panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);*/
         Image image = new Image();
         image.Read("C:\\Users\\bapti\\Downloads\\ImagesTestPGM\\ImagesTestPGM\\brain.pgm");
         System.out.println(image.image[0][1]);
